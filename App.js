51944273182086b9abf0afea6124544c50b503a1
Dumidu <dumidusumanasekara@gmail.com>
2022-05-28 21:16:55 -0400

    firebase test

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { getDatabase, ref, onValue, set } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+//import { firebaseConfig } from './firebase/firebaseConfig'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwfUY8PQltbBVkbE4i9rLODg4OBm-bdnU",
+  authDomain: "splitex-dev.firebaseapp.com",
+  projectId: "splitex-dev",
+  storageBucket: "splitex-dev.appspot.com",
+  messagingSenderId: "131845473610",
+  appId: "1:131845473610:web:81607f1082407cf933edb3",
+  measurementId: "G-DBHP349MMG"
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="testDB" onPress={storeHighScore}/>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+function storeHighScore() {
+  const db = getDatabase();
+  const reference = ref(db, 'users/' + "Dumidu");
+  set(reference, {
+    highscore: 45,
+  });
+
+  console.log(db)
 }
 
 const styles = StyleSheet.create({
