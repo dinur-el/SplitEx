9a3486c6a78f324a177a25a3aeac44e305eafe65
Dumidu <dumidusumanasekara@gmail.com>
2022-05-28 20:15:15 -0400

    firebase config created

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwfUY8PQltbBVkbE4i9rLODg4OBm-bdnU",
+  authDomain: "splitex-dev.firebaseapp.com",
+  projectId: "splitex-dev",
+  storageBucket: "splitex-dev.appspot.com",
+  messagingSenderId: "131845473610",
+  appId: "1:131845473610:web:81607f1082407cf933edb3",
+  measurementId: "G-DBHP349MMG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
