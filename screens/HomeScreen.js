3644b85c21133a709a93203568bf1ced4bb49088
Dumidu <dumidusumanasekara@gmail.com>
2022-05-30 00:44:32 -0400

    user table created pending testing the data

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,59 @@
 
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native'
 import React from 'react'
+import { useState } from 'react'
 import { auth } from '../firebaseConfig'
 import { signOut } from 'firebase/auth'
 import { useNavigation } from '@react-navigation/core'
 import { styles } from './styles/styles';
+import * as SQLite from 'expo-sqlite';
 
 
 const HomeScreen = (props) => {
 
   const navigation = useNavigation()
+
+  // create db 'splitExDB'
+  const db = SQLite.openDatabase(
+    {
+        name: 'splitExDB',
+        location: 'default'
+    }, () => { }, 
+        error => {console.log(error)}
+    );
+
+  const [users, setUsers] = useState([])
+
+  const addUserHandler = (email, id) => {
+      setUsers( userList => [...userList, { useremail:email, key:id }] )
+  }
+
+  // select users from data
+  // to be done
+  const selectSignedInUsers= () => {
+    db.transaction(
+      tx => {
+        tx.executeSql("SELECT * FROM usersTable", 
+          [], 
+          (_, { rows }) => {     
+            console.log("ROWS RETRIEVED!");
+  
+            let entries = rows._array;
+
+            entries.forEach((entry) => {
+                console.log('entries')
+                console.log(entry)
+                addUserHandler(entry.user_email, entry.id)
+            });
+          },
+          (_, result) => {
+            console.log('SELECT failed!');
+            console.log(result);
+          }
+        )
+      }
+    );
+  }  
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
@@ -19,12 +63,21 @@ const HomeScreen = (props) => {
 
   return (
       <View style={styles.container}>
-        <Text>Email: {auth.currentUser?.email}</Text>
+        {/* <FlatList 
+          data={users}
+          renderItem={({ item }) => (<Text>{item.useremail}</Text>)}
+        /> */}
         <TouchableOpacity
         style={styles.button}
         onPress={handleSignOut}
         >
           <Text style={styles.buttonText}>Sign out</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+        style={styles.button}
+        onPress={selectSignedInUsers}
+        >
+          <Text style={styles.buttonText}>Check user list</Text>
         </TouchableOpacity>
       </View>
   )
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { KeyboardAvoidingView, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles/styles';
 import { auth } from '../firebaseConfig'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import * as SQLite from 'expo-sqlite';
 
 const SignupScreen = () => {
+
+    // create db 'splitExDB'
+    const db = SQLite.openDatabase(
+        {
+            name: 'splitExDB',
+            location: 'default'
+        }, () => { }, 
+            error => {console.log(error)}
+        );
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+
+    useEffect(() => {
+        createUserTable()
+      }, 
+
+      []
+    );
+
 
     const handleSignUp = () => {
 
@@ -14,8 +34,38 @@ const SignupScreen = () => {
             .then(userCredentials => {
                 const user = userCredentials.user;
                 console.log(user.email);
+                // save user email to db
+                if(user.email){
+                    saveSignedUserData(user.email)
+                }
             })
             .catch(error => alert(error.message))
+    }
+
+
+    // create table -> usersTable
+    // columns rowid (auto), user_email (Text)
+    const createUserTable = () => {
+        db.transaction(tx => {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS usersTable (id INTEGER PRIMARY KEY AUTOINCREMENT, user_email TEXT);', 
+              [], 
+              () => console.log('TABLE CREATED!'),
+              (_, result) => console.log('TABLE CREATE failed:' + result)
+            );
+          });
+    }
+
+    const saveSignedUserData = (useremail) => {
+        db.transaction(
+            tx => {
+              // executeSql(sqlStatement, arguments, success, error)
+              tx.executeSql("INSERT INTO usersTable (user_email) values (?)", 
+                [useremail],
+                (_, { rowsAffected }) => rowsAffected > 0 ? console.log('ROW INSERTED!') : console.log('INSERT FAILED!'),
+                (_, result) => console.log('INSERT failed:' + result)
+              );
+            }    
+          );
     }
 
     return (
